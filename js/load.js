43659bcb63ac1fa08774f2de4faf254df7156abe
Ayala Bikel <ayala@example.com>
2025-09-10 12:13:48 +0300

    initial commit

---
+++ js/load.js
@@ -0,0 +1,13 @@
+document.addEventListener("DOMContentLoaded", function () {
+  fetch("includes/header.html")
+    .then(res => res.text())
+    .then(data => {
+      document.getElementById("header").innerHTML = data;
+    });
+
+  fetch("includes/footer.html")
+    .then(res => res.text())
+    .then(data => {
+      document.getElementById("footer").innerHTML = data;
+    });
+});
